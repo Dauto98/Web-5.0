7a904fc48cd39ae714e1df19ee49b1a7f10ce4cb
Dauto98 <dauto98@gmail.com>
2017-01-31 11:12:42 +0700

    add missile living status checker so that it won't calculate for the dead missile anymore

--- CI3/js/PlayerBulletType2Controller.js
+++ CI3/js/PlayerBulletType2Controller.js
@@ -6,43 +6,45 @@ class PlayerBulletType2Controller extends BulletController{
 	}
 
 	update(){
-		this.sprite.targetAngle = Nakama.game.math.angleBetween(
-			this.sprite.position.x, this.sprite.position.y,
-			Nakama.game.input.activePointer.x, Nakama.game.input.activePointer.y
-		)
+		if(this.sprite.alive){
+			this.sprite.targetAngle = Nakama.game.math.angleBetween(
+				this.sprite.position.x, this.sprite.position.y,
+				Nakama.game.input.activePointer.x, Nakama.game.input.activePointer.y
+			)
 
-		this.sprite.targetAngle += Math.PI/2;
+			this.sprite.targetAngle += Math.PI/2;
 
-		// log targetAngle, return undefined ?????
-		console.log("target angle: ",this.sprite.targetAngel);
+			// log targetAngle, return undefined ?????
+			console.log("target angle: ",this.sprite.targetAngle);
 
-		if(this.sprite.rotation !== this.sprite.targetAngle){
-			this.sprite.delta = this.sprite.targetAngle - this.sprite.rotation;
+			if(this.sprite.rotation !== this.sprite.targetAngle){
+				this.sprite.delta = this.sprite.targetAngle - this.sprite.rotation;
 
-      // Keep it in range from -180 to 180 to make the most efficient turns.
-      if (this.sprite.delta > Math.PI) {
-				this.sprite.delta -= Math.PI * 2;
+	      // Keep it in range from -180 to 180 to make the most efficient turns.
+	      if (this.sprite.delta > Math.PI) {
+					this.sprite.delta -= Math.PI * 2;
+				}
+	      if (this.sprite.delta < -Math.PI) {
+					this.sprite.delta += Math.PI * 2;
+				}
+
+	      if (this.sprite.delta > 0) {
+	          // Turn clockwise
+	          this.sprite.angle += this.sprite.TURN_RATE;
+	      } else {
+	          // Turn counter-clockwise
+	          this.sprite.angle -= this.sprite.TURN_RATE;
+	      }
+
+	      // Just set angle to target angle if they are close
+	      if (Math.abs(this.sprite.delta) < Nakama.game.math.degToRad(this.sprite.TURN_RATE)) {
+	          this.sprite.rotation = this.sprite.targetAngle;
+	      }
 			}
-      if (this.sprite.delta < -Math.PI) {
-				this.sprite.delta += Math.PI * 2;
-			}
 
-      if (this.sprite.delta > 0) {
-          // Turn clockwise
-          this.sprite.angle += this.sprite.TURN_RATE;
-      } else {
-          // Turn counter-clockwise
-          this.sprite.angle -= this.sprite.TURN_RATE;
-      }
-
-      // Just set angle to target angle if they are close
-      if (Math.abs(this.sprite.delta) < Nakama.game.math.degToRad(this.sprite.TURN_RATE)) {
-          this.sprite.rotation = this.sprite.targetAngle;
-      }
+			// Calculate velocity vector based on this.rotation and this.SPEED
+	    this.sprite.body.velocity.x = Math.cos(this.sprite.rotation - Math.PI/2) * this.sprite.SPEED;
+	    this.sprite.body.velocity.y = Math.sin(this.sprite.rotation - Math.PI/2) * this.sprite.SPEED;
 		}
-
-		// Calculate velocity vector based on this.rotation and this.SPEED
-    this.sprite.body.velocity.x = Math.cos(this.sprite.rotation - Math.PI/2) * this.sprite.SPEED;
-    this.sprite.body.velocity.y = Math.sin(this.sprite.rotation - Math.PI/2) * this.sprite.SPEED;
 	}
 }
